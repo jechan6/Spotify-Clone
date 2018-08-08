18d8dc46192e455ec24b4bd1edd97dfffc07d3b1
Jeffrey Chan <jefrayc@gmail.com>
2018-08-08 11:16:48 -0700

    Added user seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'date'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!({name: 'Jeffrey Chan', username: 'jeff', password:'123456', birthday: Date.new(2018)})
