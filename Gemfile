b4536188d9d24870ddf6fa4ca21217b9801b7dd7
Jeffrey Chan <jefrayc@gmail.com>
2018-08-08 00:31:13 -0700

    Finish Styling splash

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'puma', '~> 3.7'
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
+gem 'font-awesome-sass', '~> 5.2.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
--- frontend/components/browse/browse.jsx
+++ frontend/components/browse/browse.jsx
@@ -17,11 +17,36 @@ class Browse extends React.Component {
     );
 
     const otherGreeting = () => (
+
       <div className="browse-container">
-        <h1>Spotify Clone App</h1>
-        <div className="session-links">
-          <Link className="link-signup" to="/signup">Sign Up</Link>
-          <Link className="link-login" to="/login">Log In</Link>
+        <div className="splash-container">
+
+          <div className="left-browse">
+
+            <span className="logo"></span>
+
+
+            <div className="session-links">
+              <Link className="link-signup" to="/signup">Sign Up</Link>
+              <div className="separator">
+                <div className="separator-line"></div>
+                <div className="separator-text">ALREADY HAVE AN ACCOUNT?</div>
+                <div className="separator-line"></div>
+              </div>
+              <Link className="link-login" to="/login">Log In</Link>
+            </div>
+          </div>
+          <div className="browse-middlebar"></div>
+          <div className="right-browse">
+            <h1>Get the right music,<br/>
+            right now
+            </h1>
+            <p>Listen to millions of songs for free</p>
+            <ul>
+              <li><i class="fa fa-check" aria-hidden="true"></i>Search & discover music you'll love </li>
+              <li><i class="fa fa-check" aria-hidden="true"></i>Create playlists of your favorite music </li>
+            </ul>
+          </div>
         </div>
       </div>
     );
